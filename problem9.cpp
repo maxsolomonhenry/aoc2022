6ab182f8ded0d04d00fcf11ec3b826957f8f143d
Max Henry <mhenry@eers.ca>
2023-01-02 18:34:49 -0500

    make line parser

--- problem9.cpp
+++ problem9.cpp
@@ -4,11 +4,51 @@
 #include <string>
 #include "util.h"
 
+
+enum Aim {
+    up,
+    down,
+    left,
+    right,
+    unknown
+};
+
+Aim charToAim(const char& input)
+{
+    switch (input) {
+        case 'U':
+            return Aim::up;
+        case 'D':
+            return Aim::down;
+        case 'L':
+            return Aim::left;
+        case 'R':
+            return Aim::right;
+        default:
+            return Aim::unknown;
+    }
+}
+
+std::tuple<Aim, int> parseInstruction(const std::string& line)
+{
+    Aim aim = charToAim(line[0]);
+    int numSteps = std::stoi(line.substr(line.find(' ') + 1));
+
+    return {aim, numSteps};
+}
+
 int main() 
 {
     const std::string fpath = "input/9.txt";
     std::vector<std::string> lines;
     readFile(fpath, lines);
+
+    for (auto line : lines)
+    {
+        auto [aim, numSteps] = parseInstruction(line);
+
+        std::cout << "aim: " << aim << " numsteps: " << numSteps << "\n";
+    }
 
     return 1;
 }
